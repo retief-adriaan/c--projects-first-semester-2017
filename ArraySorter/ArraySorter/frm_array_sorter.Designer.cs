83546f00de3d94e943da5ee8288a93a93d3ee276
Adriaan Retief <retiefadriaan@gmail.com>
2017-05-03 08:20:17 +0200

    Add files via upload
    Simple interface for generating, populating and sorting arrays,

---
+++ ArraySorter/ArraySorter/frm_array_sorter.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace ArraySorter
+{
+    partial class frmArraySorter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbxOutput = new System.Windows.Forms.TextBox();
+            this.btnGenArray = new System.Windows.Forms.Button();
+            this.btnQuickSort = new System.Windows.Forms.Button();
+            this.btnBubbleSort = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.gbxSortOrder = new System.Windows.Forms.GroupBox();
+            this.rdbDescending = new System.Windows.Forms.RadioButton();
+            this.rdbAscending = new System.Windows.Forms.RadioButton();
+            this.btnDisplay = new System.Windows.Forms.Button();
+            this.nudNumElements = new System.Windows.Forms.NumericUpDown();
+            this.lblElements = new System.Windows.Forms.Label();
+            this.gbxSortOrder.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudNumElements)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tbxOutput
+            // 
+            this.tbxOutput.Location = new System.Drawing.Point(124, 12);
+            this.tbxOutput.Multiline = true;
+            this.tbxOutput.Name = "tbxOutput";
+            this.tbxOutput.ReadOnly = true;
+            this.tbxOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbxOutput.Size = new System.Drawing.Size(136, 300);
+            this.tbxOutput.TabIndex = 0;
+            // 
+            // btnGenArray
+            // 
+            this.btnGenArray.Location = new System.Drawing.Point(12, 12);
+            this.btnGenArray.Name = "btnGenArray";
+            this.btnGenArray.Size = new System.Drawing.Size(106, 23);
+            this.btnGenArray.TabIndex = 1;
+            this.btnGenArray.Text = "Generate Array";
+            this.btnGenArray.UseVisualStyleBackColor = true;
+            this.btnGenArray.Click += new System.EventHandler(this.btnGenArray_Click);
+            // 
+            // btnQuickSort
+            // 
+            this.btnQuickSort.Location = new System.Drawing.Point(12, 70);
+            this.btnQuickSort.Name = "btnQuickSort";
+            this.btnQuickSort.Size = new System.Drawing.Size(106, 23);
+            this.btnQuickSort.TabIndex = 2;
+            this.btnQuickSort.Text = "Quick Sort";
+            this.btnQuickSort.UseVisualStyleBackColor = true;
+            this.btnQuickSort.Click += new System.EventHandler(this.btnQuickSort_Click);
+            // 
+            // btnBubbleSort
+            // 
+            this.btnBubbleSort.Location = new System.Drawing.Point(12, 41);
+            this.btnBubbleSort.Name = "btnBubbleSort";
+            this.btnBubbleSort.Size = new System.Drawing.Size(106, 23);
+            this.btnBubbleSort.TabIndex = 3;
+            this.btnBubbleSort.Text = "Bubble Sort";
+            this.btnBubbleSort.UseVisualStyleBackColor = true;
+            this.btnBubbleSort.Click += new System.EventHandler(this.btnBubbleSort_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(12, 128);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(106, 23);
+            this.btnClear.TabIndex = 4;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(12, 289);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(106, 23);
+            this.btnExit.TabIndex = 5;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // gbxSortOrder
+            // 
+            this.gbxSortOrder.Controls.Add(this.rdbDescending);
+            this.gbxSortOrder.Controls.Add(this.rdbAscending);
+            this.gbxSortOrder.Location = new System.Drawing.Point(12, 157);
+            this.gbxSortOrder.Name = "gbxSortOrder";
+            this.gbxSortOrder.Size = new System.Drawing.Size(106, 61);
+            this.gbxSortOrder.TabIndex = 6;
+            this.gbxSortOrder.TabStop = false;
+            this.gbxSortOrder.Text = "Sort Order";
+            // 
+            // rdbDescending
+            // 
+            this.rdbDescending.AutoSize = true;
+            this.rdbDescending.Location = new System.Drawing.Point(3, 39);
+            this.rdbDescending.Name = "rdbDescending";
+            this.rdbDescending.Size = new System.Drawing.Size(82, 17);
+            this.rdbDescending.TabIndex = 1;
+            this.rdbDescending.TabStop = true;
+            this.rdbDescending.Text = "Descending";
+            this.rdbDescending.UseVisualStyleBackColor = true;
+            // 
+            // rdbAscending
+            // 
+            this.rdbAscending.AutoSize = true;
+            this.rdbAscending.Location = new System.Drawing.Point(3, 16);
+            this.rdbAscending.Name = "rdbAscending";
+            this.rdbAscending.Size = new System.Drawing.Size(75, 17);
+            this.rdbAscending.TabIndex = 0;
+            this.rdbAscending.TabStop = true;
+            this.rdbAscending.Text = "Ascending";
+            this.rdbAscending.UseVisualStyleBackColor = true;
+            // 
+            // btnDisplay
+            // 
+            this.btnDisplay.Location = new System.Drawing.Point(12, 99);
+            this.btnDisplay.Name = "btnDisplay";
+            this.btnDisplay.Size = new System.Drawing.Size(106, 23);
+            this.btnDisplay.TabIndex = 9;
+            this.btnDisplay.Text = "Display Array";
+            this.btnDisplay.UseVisualStyleBackColor = true;
+            this.btnDisplay.Click += new System.EventHandler(this.btnDisplay_Click);
+            // 
+            // nudNumElements
+            // 
+            this.nudNumElements.Location = new System.Drawing.Point(12, 250);
+            this.nudNumElements.Name = "nudNumElements";
+            this.nudNumElements.Size = new System.Drawing.Size(106, 20);
+            this.nudNumElements.TabIndex = 7;
+            // 
+            // lblElements
+            // 
+            this.lblElements.AutoSize = true;
+            this.lblElements.Location = new System.Drawing.Point(9, 221);
+            this.lblElements.Name = "lblElements";
+            this.lblElements.Size = new System.Drawing.Size(101, 26);
+            this.lblElements.TabIndex = 8;
+            this.lblElements.Text = "Number of elements\r\n(10-100)";
+            // 
+            // frmArraySorter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(271, 324);
+            this.Controls.Add(this.btnDisplay);
+            this.Controls.Add(this.lblElements);
+            this.Controls.Add(this.nudNumElements);
+            this.Controls.Add(this.gbxSortOrder);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnBubbleSort);
+            this.Controls.Add(this.btnQuickSort);
+            this.Controls.Add(this.btnGenArray);
+            this.Controls.Add(this.tbxOutput);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "frmArraySorter";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Array Sorter";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmArraySorter_FormClosing);
+            this.gbxSortOrder.ResumeLayout(false);
+            this.gbxSortOrder.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudNumElements)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbxOutput;
+        private System.Windows.Forms.Button btnGenArray;
+        private System.Windows.Forms.Button btnQuickSort;
+        private System.Windows.Forms.Button btnBubbleSort;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.GroupBox gbxSortOrder;
+        private System.Windows.Forms.RadioButton rdbDescending;
+        private System.Windows.Forms.RadioButton rdbAscending;
+        private System.Windows.Forms.Button btnDisplay;
+        private System.Windows.Forms.NumericUpDown nudNumElements;
+        private System.Windows.Forms.Label lblElements;
+    }
+}
+
---
+++ ArraySorter/ArraySorter/frm_array_sorter.cs
@@ -0,0 +1,180 @@
+﻿// created by ADRIAAN RETIEF on 16/03/2017
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArraySorter
+{
+    public partial class frmArraySorter : Form
+    {
+        public frmArraySorter()
+        {
+            // initialize form
+            InitializeComponent();
+            // initialize button states
+            btnGenArray.Enabled = true;
+            btnBubbleSort.Enabled = false;
+            btnQuickSort.Enabled = false;
+            btnClear.Enabled = false;
+            btnExit.Enabled = true;
+            // initialize radio buttons, set default
+            rdbAscending.Enabled = true;
+            rdbDescending.Enabled = true;
+            rdbAscending.Checked = true;
+            rdbDescending.Checked = false;
+            // set range for nud
+            nudNumElements.Minimum = 10;
+            nudNumElements.Maximum = 100;
+            nudNumElements.Value = 10;
+            // clear output
+            tbxOutput.Clear();
+
+            // disable nud and label because the associated code deosn't work
+            nudNumElements.Enabled = false;
+            lblElements.Enabled = false;
+        }
+
+        // global constants 
+        //const int NUM_ELEMENTS = nudNumElements.Value;
+        const int NUM_ELEMENTS = 40;
+        const int LOW_BOUND = 0;
+        const int UPP_BOUND = 100;
+        // array 
+        int[] elements = new int[NUM_ELEMENTS];
+        // random element
+        Random rand = new Random();
+
+        private void btnGenArray_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < NUM_ELEMENTS; i++)
+            {
+                elements[i] = rand.Next(LOW_BOUND, UPP_BOUND);
+            }
+            DisplayArray("Array before sorting:");
+            // enable sorting and clear buttons
+            btnBubbleSort.Enabled = true;
+            btnQuickSort.Enabled = true;
+            btnClear.Enabled = true;
+        }
+
+        public void DisplayArray(string state)
+        {
+            // write contents of array to output
+            tbxOutput.Clear();
+            tbxOutput.Text += state;
+            for (int i = 0; i < NUM_ELEMENTS; i++)
+            {
+                tbxOutput.Text += "\r\n" + elements[i];
+            }            
+        }
+
+        private void btnBubbleSort_Click(object sender, EventArgs e)
+        {
+            int temp = 0;
+            bool swap = true;
+
+            // bubble sort process
+            while (swap)
+            {
+                swap = false;
+                for (int i = 0; i < NUM_ELEMENTS-1; i++)
+                {
+                    if (rdbAscending.Checked)
+                    {
+                        if (elements[i] > elements[i + 1])
+                        {
+                            temp = elements[i];
+                            elements[i] = elements[i + 1];
+                            elements[i + 1] = temp;
+                            swap = true;
+                        }
+                    }
+                    else
+                    {
+                        if (elements[i] < elements[i + 1])
+                        {
+                            temp = elements[i];
+                            elements[i] = elements[i + 1];
+                            elements[i + 1] = temp;
+                            swap = true;
+                        }
+                    }
+                }
+            }
+            btnBubbleSort.Enabled = false;
+            btnQuickSort.Enabled = false;
+            DisplayArray("Array after sorting:");
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            // resets button states, clears output, nullifies array
+            tbxOutput.Clear();
+            btnGenArray.Enabled = true;
+            btnQuickSort.Enabled = false;
+            btnBubbleSort.Enabled = false;
+            btnClear.Enabled = false;
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnQuickSort_Click(object sender, EventArgs e)
+        {
+            QuickSort(0, NUM_ELEMENTS-1);
+            btnBubbleSort.Enabled = false;
+            btnQuickSort.Enabled = false;
+            DisplayArray("Array after sorting");
+        }
+
+        public void QuickSort(int left, int right)
+        {
+            int i = left, j = right;
+            int pivot = elements[(left + right) / 2];
+
+            // quick sort process
+            while (i <= j)
+            {
+                if (rdbAscending.Checked)
+                {
+                    while (elements[i].CompareTo(pivot) < 0) i++;
+                    while (elements[j].CompareTo(pivot) > 0) j--;
+                }
+                else
+                {
+                    while (elements[i].CompareTo(pivot) > 0) i++;
+                    while (elements[j].CompareTo(pivot) < 0) j--;
+                }
+
+                if (i <= j)
+                {
+                    int temp = elements[i];
+                    elements[i] = elements[j];
+                    elements[j] = temp;
+                    i++; j--;
+                }
+            }
+            if (left < j) QuickSort(left, j);
+            if (right > i) QuickSort(i, right);
+        }
+
+        private void btnDisplay_Click(object sender, EventArgs e)
+        {
+            DisplayArray("Array in its current state:");
+        }
+
+        private void frmArraySorter_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
